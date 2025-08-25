e27265037a7ce6413748e3e73a50ef98e58525fe
Alper YILDIRIM <73199742+Naganen@users.noreply.github.com>
2025-08-25 23:55:56 +0300

    Add .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js 1756155355098

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_20e538a0._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_f4354cdd._.js",
+  "static/chunks/node_modules_335b2a1f._.js"
+],
+    source: "dynamic"
+});
